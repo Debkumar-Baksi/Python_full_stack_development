88400f10f2d4b1548acfcf911e0a960991b0bb37
Debkumar_Baksi <sg383332@gmail.com>
2025-02-19 19:10:01 +0530

    updated

--- Todo_webapp/todo_site/todo_site/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from todo import views
-
-urlpatterns = [
-    path('',views.index,name="todo"),
-    path('del/<str:item_id>',views.remove,name='del'),
-    path('admin/', admin.site.urls),
-]
